2b9afc8ca8291d58b6287cb9d7df5418ddf5837f
FrancescoDAlterio <francesco.dalterio1992@gmail.com>
2020-03-03 19:45:05 +0100

    developed check convergence process for a db single file

--- imports/manipulator.py
+++ imports/manipulator.py
@@ -23,7 +23,7 @@ class Manipulator(metaclass=Singleton):
                 err_mess = 'ERROR WHILE PROCESSING SUBDIR: ' + str(subdir)
                 err_details = 'ErroDetails: ' + res[1]
                 raise Exception(err_code, err_mess, err_details)
-            break
+
 
 
 
@@ -42,15 +42,15 @@ class Manipulator(metaclass=Singleton):
         for db_file in res[1]:
 
             #todo manage result!!!
-            res = self.__manageDBFile(db_file)
+            res = self.__performDbOperations(db_file)
 
-
+            print('REEEEES ',res)
 
         return 0, None
 
 
 #SELECT * FROM events JOIN typeEvent_message_sent ON events.event_id = typeEvent_message_sent.event_id WHERE( events.submitter_id='AAAAA' and events.type = 0 and typeEvent_message_sent.message_type=0)
-    def __manageDBFile(self,db_file):
+    def __performDbOperations(self,db_file):
         print('***2 start db_file ',db_file)
         db_manager = DatabaseManager(db_file)
 
@@ -67,39 +67,78 @@ class Manipulator(metaclass=Singleton):
         print('USERLIST: ',userList)
 
         total_num_sent_update_msg = 0
+        highest_convercence_times_list=[]
 
         for user in userList:
             res = db_manager.getSentMessageEvents(user,0)
             if res[0] != 0:
                 err_code = '3MAN'  # MAN stands for Manipulator
                 err_mess = 'ERROR WHILE getting Sent Message Events from DB: '
-                err_details = 'ErroDetails: ' + res[1]
+                err_details = 'ErrorDetails: ' + res[1]
                 raise Exception(err_code, err_mess, err_details)
 
             user_sent_update_msg_list = res[1]
             total_num_sent_update_msg = total_num_sent_update_msg + len(user_sent_update_msg_list)
 
+            convergence_times = []
             for msg in user_sent_update_msg_list:
                 payload = msg[-1]
                 res = self.__checkConvergenceMessage(userList,payload)
 
-            user_rcv_update_msg = ''
+                if res[1]:
+                    convergence_times.append(msg[2])
 
-        return 0,None
+            res = db_manager.getReceivedMessageEvents(user, 0)
+            if res[0] != 0:
+                err_code = '4MAN'  # MAN stands for Manipulator
+                err_mess = 'ERROR WHILE getting Received Message Events from DB: '
+                err_details = 'ErrorDetails: ' + res[1]
+                raise Exception(err_code, err_mess, err_details)
+
+            user_rcv_update_msg_list = res[1]
+
+            for msg in user_rcv_update_msg_list:
+                payload = msg[-1]
+                res = self.__checkConvergenceMessage(userList,payload)
+
+                if res[1]:
+                    #can throw exceptions
+                    convergence_times.append(int(msg[2]))
+
+            if len(convergence_times) == 0:
+                return 0,False
+
+            highest_convercence_times_list.append(max(convergence_times))
+
+        res = db_manager.getEarliestDeviceUpEvent()
+
+        if res[0] != 0:
+            err_code = '5MAN'  # MAN stands for Manipulator
+            err_mess = 'ERROR WHILE getting Earliest Device-up Event from DB: '
+            err_details = 'ErrorDetails: ' + res[1]
+            raise Exception(err_code, err_mess, err_details)
+
+        print('#### ',res[1])
+
+        earliest_device_up_timestamp = res[1][0][2]
+        max_convergence_time = int(max(highest_convercence_times_list)) - int(earliest_device_up_timestamp)
+
+        print('first device-up ts: ', res[1][0][2], ' - max convergence time: ', max(highest_convercence_times_list),
+              ' - total_num_sent_update_msg: ', total_num_sent_update_msg, ' - max_convergence_time: ',max_convergence_time)
+
+        return 0,True,max_convergence_time,total_num_sent_update_msg
 
 
     def __checkConvergenceMessage(self,userList,payload):
 
         pl_split = payload.split('-')
+        uL = list(userList)
 
         for index, elem in enumerate(pl_split):
 
-            if elem in userList:
-                userList.remove(elem)
-
-        print('ééé ',userList)
-
-        if len(userList) == 0:
+            if elem in uL:
+                uL.remove(elem)
+        if len(uL) == 0:
             return 0,True
 
         return 0,False
--- imports/utilities/databaseManager.py
+++ imports/utilities/databaseManager.py
@@ -27,7 +27,7 @@ class DatabaseManager():
 
         return 0,users
 
-    def getSentMessageEvents(self,device_id,type):
+    def getSentMessageEvents(self,device_id,mess_type):
         self.__conn = sqlite3.connect(self.__sql_database_path)
         cur = self.__conn.cursor()
 
@@ -36,8 +36,8 @@ class DatabaseManager():
 
             cur.execute("SELECT * FROM {tn} "
                         "JOIN typeEvent_message_sent ON events.event_id = typeEvent_message_sent.event_id "
-                        "WHERE( events.submitter_id='{submitter_id}' and events.type = {type} and typeEvent_message_sent.message_type=0)". \
-                        format(tn='events', submitter_id=device_id, type=type))
+                        "WHERE( events.submitter_id='{submitter_id}' and events.type = 0 and typeEvent_message_sent.message_type='{mess_type}')". \
+                        format(tn='events', submitter_id=device_id, mess_type=mess_type))
 
             query_result = cur.fetchall()
 
@@ -45,7 +45,50 @@ class DatabaseManager():
             self.__conn.close()
             message = 'SQL Error ' + str(e)
             return 1, message
+        return 0, query_result
+
+    def getReceivedMessageEvents(self, device_id, mess_type):
+        self.__conn = sqlite3.connect(self.__sql_database_path)
+        cur = self.__conn.cursor()
+
+        try:
+            # SELECT * FROM events JOIN typeEvent_message_received ON events.event_id = typeEvent_message_received.event_id WHERE( events.submitter_id='AAAAA' and events.type = 0 and typeEvent_message_received.message_type=0)
+
+            cur.execute("SELECT * FROM {tn} "
+                        "JOIN typeEvent_message_received ON events.event_id = typeEvent_message_received.event_id "
+                        "WHERE( events.submitter_id='{submitter_id}' and events.type = 1 and typeEvent_message_received.message_type='{mess_type}')". \
+                        format(tn='events', submitter_id=device_id, mess_type=mess_type))
+
+            query_result = cur.fetchall()
+
+        except sqlite3.Error as e:
+            self.__conn.close()
+            message = 'SQL Error ' + str(e)
+            return 1, message
+        return 0, query_result
 
 
+    def getEarliestDeviceUpEvent(self):
+        self.__conn = sqlite3.connect(self.__sql_database_path)
+        cur = self.__conn.cursor()
 
+        try:
+
+            cur.execute("SELECT tbl1.* "
+                        "FROM events tbl1"
+                        "  JOIN"
+                        "  ("
+                        "   SELECT submitter_id, MIN(timestamp) MinTS"
+                        "   FROM events"
+                        "  )tbl2 "
+                        "  ON tbl2.submitter_id = tbl1.submitter_id "
+                        "WHERE tbl2.MinTS = tbl1.timestamp". \
+                        format(tn='events'))
+
+            query_result = cur.fetchall()
+
+        except sqlite3.Error as e:
+            self.__conn.close()
+            message = 'SQL Error ' + str(e)
+            return 1, message
         return 0, query_result
