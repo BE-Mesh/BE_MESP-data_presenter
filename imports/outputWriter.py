ba6a5578376ecca0078e119eaa9d1dc31d553bdf
FrancescoDAlterio <francesco.dalterio1992@gmail.com>
2020-03-04 15:38:17 +0100

    implemented graph generation for graph-studio and plotly v3.0.0, NB use plotly v3.0.0

--- imports/outputWriter.py
+++ imports/outputWriter.py
@@ -1,10 +1,21 @@
 from .utilities.singleton import Singleton
 from .utilities.outputDirectoryManager import OutputDirectoryManager
+
+#plotly
 import plotly
 import plotly.graph_objs as go
 
+
+#chart-studio
+# import chart_studio.plotly as py
+# import plotly.io as pio
+# import chart_studio
+# import plotly.graph_objs as go
+
+
 class OutputWriter(metaclass=Singleton):
     def __init__(self):
+        #chart_studio.tools.set_credentials_file(username='Francesco_DA', api_key='2C8Wr0IdIC7EO5kqS2PT')
 
         self.__output_dir_path = OutputDirectoryManager().getOutputDir()
         self.__timestamp_plot_path = self.__output_dir_path + '/' + 'SubcaseXtimestamp'
@@ -38,8 +49,14 @@ class OutputWriter(metaclass=Singleton):
         data = [trace]
 
         fig = go.Figure(data=data, layout=layout)
+
+        #plotly
         plotly.offline.plot(fig, filename=self.__timestamp_plot_path)
 
+
+        #chart-studio
+        #res = pio.write_html(fig, file=self.__timestamp_plot_path, auto_open=True)
+        #print('RES ',res)
         return 0,None
 
     def createSubcaseXnumUpdatePackets(self,subcases_list):
@@ -71,7 +88,16 @@ class OutputWriter(metaclass=Singleton):
         data = [trace]
 
         fig = go.Figure(data=data, layout=layout)
+
+        #plotly
         plotly.offline.plot(fig, filename=self.__num_update_msg_plot_path)
+
+
+
+        #graph-studio
+        #offline #pio.write_html(fig, file=self.__num_update_msg_plot_path, auto_open=True)
+        #online #py.plot(fig, filename=self.__num_update_msg_plot_path ,auto_open=True,sharing='public')
+        #plotly.offline.plot(fig, filename=self.__num_update_msg_plot_path)
 
         return 0,None
 
